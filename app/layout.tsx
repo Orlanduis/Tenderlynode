4ac7138357fd63c2b27132405f85667f21faa760
Orlanduis <153502813+Orlanduis@users.noreply.github.com>
2024-09-27 03:44:17 -0600

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+'use client';
+ 
+import './globals.css';
+import { EthereumWalletConnectors } from '@dynamic-labs/ethereum';
+import { tenderlyChains } from '@/tenderly.config';
+import { DynamicContextProvider, DynamicWidget } from '@dynamic-labs/sdk-react-core';
+import { DynamicWagmiConnector } from '@dynamic-labs/wagmi-connector';
+ 
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
+  return (
+    <html lang="en">
+    <DynamicContextProvider
+      settings={{
+        // Find your environment id at https://app.dynamic.xyz/dashboard/developer
+        environmentId: 'ENVIRONMENT_ID',
+        walletConnectors: [EthereumWalletConnectors],
+        evmNetworks: process.env.NEXT_PUBLIC_TENDERLY_VNETS_ENABLED ? tenderlyChains : [],
+      }}
+    >
+      <DynamicWagmiConnector>
+        <body>
+        <DynamicWidget />
+        {children}
+        </body>
+      </DynamicWagmiConnector>
+    </DynamicContextProvider>
+    </html>
+ 
+  );
+}
