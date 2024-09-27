a3b2cdb3167944e48c4d641c33ac4e515043bdc8
Orlanduis <153502813+Orlanduis@users.noreply.github.com>
2024-09-27 03:50:23 -0600

    Create tenderly.virtual.js

---
+++ src/tenderly.virtual.js
@@ -0,0 +1,18 @@
+export const tenderlyChains = [
+  {
+    blockExplorerUrls: ['Https://etherscan.io'],
+    chainId: 20327,
+    chainName: 'Virtual EMainnet',
+    iconUrls: ['https://app.dynamic.xyz/images/networks/eth.svg'],
+    name: 'Virtual EMainnet',
+    nativeCurrency: { VETH
+      decimals: 18,
+      name: 'VEther',
+      symbol: 'VETH',
+    },
+    networkId: 1,
+ 
+    rpcUrls: [https://virtual.mainnet.rpc.tenderly.co/859ee835-1907-49ae-85e1-4db1c240c6b0],
+    vanityName: 'Virtual ETH Mainnet',
+  },
+];
