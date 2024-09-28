48cb9f27b957b8212803c6b80767c71f7f4a7301
Orlanduis <153502813+Orlanduis@users.noreply.github.com>
2024-09-28 02:12:14 -0600

    Update Ethers.js

--- Ethers.js
+++ Ethers.js
@@ -23,3 +23,19 @@ const signer = Wallet.fromPhrase(Mnemonic.fromEntropy(ethers.randomBytes(24)).ph
   console.error(e);
   process.exitCode = 1;
 });
+import { ethers } from 'ethers';
+
+const provider = new ethers.JsonRpcProvider('https://virtual.mainnet.rpc.tenderly.co/9ff5bc0e-57bb-41cd-8ef8-3e34d6b70c7f');
+const EXPLORER_BASE_URL = "https://virtual.mainnet.rpc.tenderly.co/859ee835-1907-49ae-85e1-4db1c240c6b0";
+
+(async () => {
+  const tx = await provider.send('tenderly_setBalance', [
+      ['0x0d2026b3EE6eC71FC6746ADb6311F6d3Ba1C000B', '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'],
+      '0xDE0B6B3A7640000',
+    ]);
+
+  console.log(`${EXPLORER_BASE_URL}/tx/${tx.hash}`);
+})().catch(e => {
+  console.error(e);
+  process.exitCode = 1;
+});
