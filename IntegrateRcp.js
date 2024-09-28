aa0df50233f921120f5af4e8e087324b41966ac4
Orlanduis <153502813+Orlanduis@users.noreply.github.com>
2024-09-28 02:22:28 -0600

    Update IntegrateRcp.js

--- IntegrateRcp.js
+++ IntegrateRcp.js
@@ -6,7 +6,12 @@ tenderly.setup({ automaticVerifications: true });
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
-  networks: {
+  networks: {  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia,
     virtual_mainnet: {
       url: "https://virtual.mainnet.rpc.tenderly.co/9ff5bc0e-57bb-41cd-8ef8-3e34d6b70c7f",
       chainId: 20327,
