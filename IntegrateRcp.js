40d44fa14a5a374b8a71a4f8405ffcfe475aea56
Orlanduis <153502813+Orlanduis@users.noreply.github.com>
2024-09-28 02:21:20 -0600

    Create IntegrateRcp.js

---
+++ IntegrateRcp.js
@@ -0,0 +1,23 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as tenderly from "@tenderly/hardhat-tenderly";
+
+tenderly.setup({ automaticVerifications: true });
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+  networks: {
+    virtual_mainnet: {
+      url: "https://virtual.mainnet.rpc.tenderly.co/9ff5bc0e-57bb-41cd-8ef8-3e34d6b70c7f",
+      chainId: 20327,
+      currency: "VETH"
+    },
+  },
+  tenderly: {
+    // https://docs.tenderly.co/account/projects/account-project-slug
+    project: "rolistoken",
+    username: "Antiproject",
+  },
+};
+
+export default config;
