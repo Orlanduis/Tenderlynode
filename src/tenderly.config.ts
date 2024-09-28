454f3ddca1a4b26f4849e34d1a7f875c839e4648
Orlanduis <153502813+Orlanduis@users.noreply.github.com>
2024-09-27 18:31:58 -0600

    Update tenderly.config.ts

--- src/tenderly.config.ts
+++ src/tenderly.config.ts
@@ -17,14 +17,14 @@ export const vMainnet = defineChain({
   },
   contracts: {
     ensRegistry: {
-      address: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e'
+      address: '0x4200000000000000000000000000000000000006'
     },
     ensUniversalResolver: {
-      address: '0xE4Acdd618deED4e6d2f03b9bf62dc6118FC9A4da',
+      address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
       blockCreated: 16773775
     },
     multicall3: {
-      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      address: '0x0000000000000000000000000000000000000000',
       blockCreated: 14353601
     }
   }
